51637981b3f7b3fca14e024e16eef593fe06ce78
mao <1296193245@qq.com>
2022-08-11 20:27:44 +0800

    java_design_patterns_synthetic_reuse_principle

---
+++ src/main/java/mao/before/Car.java
@@ -0,0 +1,22 @@
+package mao.before;
+
+/**
+ * Project name(项目名称)：java设计模式_合成复用原则
+ * Package(包名): mao.before
+ * Class(类名): Car
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/8/11
+ * Time(创建时间)： 20:20
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class Car
+{
+    public void move()
+    {
+        System.out.println("汽车移动");
+    }
+}
---
+++ src/main/java/mao/before/WhiteElectricCar.java
@@ -0,0 +1,23 @@
+package mao.before;
+
+/**
+ * Project name(项目名称)：java设计模式_合成复用原则
+ * Package(包名): mao.before
+ * Class(类名): WhiteElectricCar
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/8/11
+ * Time(创建时间)： 20:26
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class WhiteElectricCar extends ElectricCar
+{
+    @Override
+    public void move()
+    {
+        System.out.println("红色电动汽车移动");
+    }
+}
